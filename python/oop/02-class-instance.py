a83c32b6937d8faec11fd33725f773cb76346dc8
nipuran <nipuransarania@gmail.com>
2024-03-23 02:55:21 +0530

    Set a default value to end_date argument

--- python/oop/02-class-instance.py
+++ python/oop/02-class-instance.py
@@ -1,12 +1,13 @@
 import datetime
 
 class Anime:
-    def __init__(self, name: str, episodes: int, duration: int, air_date: str, end_date: str='Jan 1, 1111'):
+    def __init__(self, name: str, episodes: int, duration: int, air_date: str, end_date: str=None):
         self.name = name
         self.episodes = episodes
         self.duration = datetime.time(0, duration)
         self.air_date = datetime.datetime.strptime(air_date, '%b %d, %Y')
-        self.end_date = datetime.datetime.strptime(end_date, '%b %d, %Y')
+        if end_date is not None:
+            self.end_date = datetime.datetime.strptime(end_date, '%b %d, %Y')
 
     def __str__(self):
         return 'The class object encapsulates various details about a specific anime series'
@@ -31,4 +32,6 @@ if __name__ == '__main__':
     id_2 = Anime('Classroom of the Elite III', 13, 23, 'Jan 3, 2024', 'Mar 27, 2024')
     id_3 = Anime('Delicious in Dungeon', 24, 25, 'Jan 4, 2024')
 
+    print(id_2.__dict__)
+    print(id_3.__dict__)
     id_3.expected_end_date()
